901eb62794bcc739d90d33ddd130088d7b3ff075
Manu1mohan <mohanmechrocks@gmail.com>
2024-11-27 06:30:13 +0530

    added discriptin to print

--- arthm.sh
+++ arthm.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 echo "Enter num1"
 read a
 echo "Enter num2"
 read b
 
 sum=$((" $a + $b "))
-echo $sum
+echo "addition of the above number is :$sum"
 
 sub=$((" $a - $b "))
-echo $sub
+echo "substration of the above number is :$sub"
 
 mul=$((" $a * $b "))
-echo $mul
+echo "Multplication of the above number is :$mul"
 
 if [ $a -gt $b ]
 then
@@ -19,3 +19,4 @@ div=$((" $a / $b "))
 else
 div=$((" $b / $a "))
 fi
+echo "Division  of the above number is :$div"
